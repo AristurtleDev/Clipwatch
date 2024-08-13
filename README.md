582c711b246c04b25c5276addc37b70daa8b63ac
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2024-08-13 02:59:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Clipwatch
+Provides toast notifications on windows when a CTRL+C copy to clipboard is successful
+
+## Why was this made?
+Because I posted this in the MonoGame discord
+
+![](./image.png)
+
+Which sparked a conversation on Windows needing to notify when a copy is successful, so I said why the hell not.
+
+## License
+This is licensed under the Unlicense.  Refer to the [LICENSE](./LICENSE) file for license text.
---
+++ source/ClipWatch/Game1.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Windows.Forms;
+
+namespace ClipWatch;
+
+public class Game1 : Game
+{
+    [DllImport("user32.dll")]
+    private static extern uint GetClipboardSequenceNumber();
+
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private uint _previousSequenceNumber;
+
+
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        ToastNotificationManagerCompat.OnActivated += args =>
+        {
+            //  Do nothing
+        };
+        _previousSequenceNumber = GetClipboardSequenceNumber();
+        
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        uint currentSequenceNumber = GetClipboardSequenceNumber();
+        if(_previousSequenceNumber != currentSequenceNumber)
+        {
+            _previousSequenceNumber = currentSequenceNumber;
+           new ToastContentBuilder().AddText("Clipboard Copy Successful").Show();
+        }
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Black);
+    }
+}
